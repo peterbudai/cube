7dc1b525e8c6e21f35b5725701fb2438c2464d5a
Péter Budai <buc@peterbudai.eu>
2016-05-01 22:31:42 +0200

    Commented and refactored cube logic

--- firmware/src/cube.c
+++ firmware/src/cube.c
@@ -1,10 +1,15 @@
-#include "cube.h"
+/// @file cube.c
+/// @copyright (C) 2016 Peter Budai
+/// LED cube peripherial control code.
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/atomic.h>
 
-// Port numbers and bits
+#include "cube.h"
+
+/// @name Port numbers and bits.
+/// @{
 #define ROWH_PORT PORTD
 #define ROWH_MASK ((1 << PORTD7) | (1 << PORTD6) | (1 << PORTD5) | (1 << PORTD4))
 #define ROWL_PORT PORTC
@@ -20,13 +25,10 @@
 
 #define LAYER_PORT PORTB
 #define LAYER_MASK ((1 << PORTB2) | (1 << PORTB1) | (1 << PORTB0))
+/// @}
 
-#define FRAME_REPEAT 5
-#define FRAME_PER_SECOND 25
-
-uint8_t frame_buffer[CUBE_FRAME_SIZE * CUBE_FRAME_BUFFER_COUNT] __attribute__((section(".noinit")));
-
-// Macros for dealing with output ports
+/// @name Macros for dealing with output ports.
+/// @{
 #define enable_off() ENABLE_PORT |= ENABLE_BIT
 #define enable_on() ENABLE_PORT &= ~ENABLE_BIT
 
@@ -39,18 +41,49 @@ uint8_t frame_buffer[CUBE_FRAME_SIZE * CUBE_FRAME_BUFFER_COUNT] __attribute__((s
 
 #define layer_select(l) LAYER_PORT = (LAYER_PORT & ~(LAYER_MASK)) | ((l) & LAYER_MASK)
 #define layer_address(l) ((l) * 8)
+/// @}
 
+/// How many times a full frame is displayed before advancing to the next one.
+#define FRAME_REPEAT 5
+/// How many frames will be displayed in a second.
+#define FRAME_PER_SECOND 25
+
+/// Calculates the address of current frame.
+/// @param f The current frame index.
 #define frame_address(f) (frame_buffer + (f) * CUBE_FRAME_SIZE)
+/// Calculates the address of the next frame.
+/// @param f The current frame index.
 #define frame_next(f) ((f) >= CUBE_FRAME_BUFFER_COUNT - 1 ? 0 : (f) + 1)
 
-uint8_t current_layer __attribute__((section(".noinit")));		// 0..7
-uint8_t current_repeat __attribute__((section(".noinit")));		// 0..FRAME_REPEAT
-uint8_t current_frame __attribute__((section(".noinit")));		// 0..CUBE_FRAME_BUFFER_COUNT
-uint8_t edited_frame __attribute__((section(".noinit")));		// 0..CUBE_FRAME_BUFFER_COUNT
+/// @name Global variables.
+/// @{
 
+/// Frame buffer.
+/// This is a circular buffer for CUBE_FRAME_BUFFER_COUNT number of full 3D frames.
+uint8_t frame_buffer[CUBE_FRAME_SIZE * CUBE_FRAME_BUFFER_COUNT] __attribute__((section(".noinit")));
+/// Current layer being multiplexed.
+/// Possible values: 0..7
+uint8_t current_layer __attribute__((section(".noinit")));
+/// Current iteration number of the currently displayed 3D frame.
+/// Possible values: 0..FRAME_REPEAT
+uint8_t current_repeat __attribute__((section(".noinit")));
+/// The index of the currently displayed 3D frame in the frame buffer.
+/// Possible values: 0..CUBE_FRAME_BUFFER_COUNT
+uint8_t current_frame __attribute__((section(".noinit")));
+/// The index of the currently edited 3D frame in the frame buffer.
+/// Possible values: 0..CUBE_FRAME_BUFFER_COUNT
+uint8_t edited_frame __attribute__((section(".noinit")));
+/// How many frames are filled in the buffer (including the displayed one, but not the edited one).
+/// Possible values: 0..CUBE_FRAME_BUFFER_COUNT
+uint8_t frame_count __attribute__((section(".noinit")));
+
+/// @}
+
+/// Timer interrupt handler.
+/// This will fire once the next layer has to be displayed.
 ISR(TIMER0_COMPA_vect) {
+	// Display the current layer of the current frame
 	uint8_t* layer = frame_address(current_frame) + layer_address(current_layer);
-
 	enable_off();
 	layer_select(current_layer);
 	for(uint8_t row = 0; row < 8; row++) {
@@ -62,20 +95,23 @@ ISR(TIMER0_COMPA_vect) {
 	store();
 	enable_on();
 
+	// Advance to the next layer, iteration or frame
 	current_layer++;
 	if(current_layer >= 8) {
 		current_layer = 0;
 		current_repeat++;
 		if(current_repeat >= FRAME_REPEAT) {
 			current_repeat = 0;
-			uint8_t next_frame = frame_next(current_frame);
-			if(next_frame != edited_frame) {
-				current_frame = next_frame;
+			// If there are no more frames to display, the last one will be freezed
+			if(frame_count > 1) {
+				current_frame = frame_next(current_frame);
+				frame_count--;
 			}
 		}
 	}
 }
 
+/// Clears the currently edited frame
 static void cube_clear_edited_frame(void) {
 	uint8_t* frame = frame_address(edited_frame);
 	for(uint8_t i = 0; i < CUBE_FRAME_SIZE; i++) {
@@ -83,6 +119,8 @@ static void cube_clear_edited_frame(void) {
 	}
 }
 
+/// Copies the contents of the currently edited frame to the next one.
+/// @param next Index of the next frame.
 static void cube_copy_edited_frame(uint8_t next) {
 	uint8_t* prev_frame = frame_address(edited_frame);
 	uint8_t* next_frame = frame_address(next);
@@ -93,35 +131,41 @@ static void cube_copy_edited_frame(uint8_t next) {
 
 void cube_init(void)
 {
+	// Init I/O ports
 	DDRB |= LAYER_MASK;
 	DDRC |= (ROWL_MASK | SHIFT_BIT | STORE_BIT);
 	DDRD |= (ROWH_MASK | ENABLE_BIT);
 
+	// Start with an empty frame
 	current_frame = 0;
 	edited_frame = 0;
-
 	cube_clear_edited_frame();
 	cube_advance_frame(CUBE_FRAME_CLEAR);
 
-	TCCR0A = 2;		// Set CTC mode
-	OCR0A = 0x7C;	// Set interval to 1000 Hz
+	// Set CTC mode
+	TCCR0A = 0x02;
+	// Set interval to approx 1000 Hz
+	OCR0A = (F_CPU / 64 / (FRAME_PER_SECOND * FRAME_REPEAT * 8)) - 1;
 }
 
-bool cube_advance_frame(uint8_t method) {
+uint8_t cube_advance_frame(uint8_t method) {
+	uint8_t free_count;
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		uint8_t next_frame = frame_next(edited_frame);
-		if(next_frame == current_frame) {
-			return false;
-		}
-		if(method == CUBE_FRAME_COPY) {
-			cube_copy_edited_frame(next_frame);
-		}
-		edited_frame = next_frame;
-		if(method == CUBE_FRAME_CLEAR) {
-			cube_clear_edited_frame();
+		free_count = CUBE_FRAME_BUFFER_COUNT - frame_count;
+		// If there are no more undisplayed frames, the current one remains being edited
+		if(free_count > 0) {
+			uint8_t next_frame = frame_next(edited_frame);
+			if(method == CUBE_FRAME_COPY) {
+				cube_copy_edited_frame(next_frame);
+			}
+			edited_frame = next_frame;
+			frame_count++;
+			if(method == CUBE_FRAME_CLEAR) {
+				cube_clear_edited_frame();
+			}
 		}
 	}
-	return true;
+	return free_count;
 }
 
 uint8_t* cube_get_frame(void) {
@@ -131,9 +175,13 @@ uint8_t* cube_get_frame(void) {
 void cube_disable(void)
 {
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		TCCR0B = 0;		// Set clock source to OFF
-		TIMSK0 = 0;		// Disable timer interrupt
-		TIFR0 = 2;		// Clear interrupt flag if any
+		// Set clock source to OFF
+		TCCR0B = 0x00;
+		// Disable timer interrupt
+		TIMSK0 = 0x00;
+		// Clear interrupt flag if any
+		TIFR0 = 0x02;
+		// Turn cube off
 		enable_off();
 	}
 }
@@ -141,11 +189,15 @@ void cube_disable(void)
 void cube_enable(void)
 {
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		TIMSK0 = 2;		// Enable timer interrupt
-		TCCR0B = 3;		// Set clock source to F_CPU/64
-		TCNT0 = 0;		// Reset timer
-
+		// Enable timer interrupt
+		TIMSK0 = 0x02;
+		// Set clock source to F_CPU/64
+		TCCR0B = 0x03;
+		// Reset timer
+		TCNT0 = 0x00;
+		// Start with a whole frame, cube will be enabled when timer fires
 		current_layer = 0;
 		current_repeat = 0;
 	}
 }
+
--- firmware/src/cube.h
+++ firmware/src/cube.h
@@ -1,40 +1,47 @@
+/// @file cube.h
+/// @copyright (C) 2016 Peter Budai
+/// Internal interface for the LED cube control peripherial.
+
 #ifndef CUBE_H
 #define CUBE_H
 
 #include <stdint.h>
-#include <stdbool.h>
 
-/**
- * Size of one 3D frame in bytes.
- */
+/// Size of one 3D frame in bytes.
 #define CUBE_FRAME_SIZE 64
-/**
- * Number of frame buffers.
- */
+/// Number of frame buffers.
 #define CUBE_FRAME_BUFFER_COUNT 16
 
+/// @name Cube frame advancing methods.
+/// @{
+
+/// Keep the edited frame as-is.
 #define CUBE_FRAME_ASIS 0
+/// Clear the edited frame.
 #define CUBE_FRAME_CLEAR 1
+/// Copy previous frame to the edited one.
 #define CUBE_FRAME_COPY 2
 
-/**
- * Initializes the cube ports and interrupts.
- * The cube is turned off by default.
- */
+/// @}
+
+/// Initializes the cube ports and interrupts.
+/// The cube is turned off by default.
 void cube_init(void);
 
-bool cube_advance_frame(uint8_t method);
+/// Close the currently edited frame and advance to the next one.
+/// @param method What to do with the next frame.
+/// @return The number of frames left (including the currently edited one).
+uint8_t cube_advance_frame(uint8_t method);
 
+/// Return the currently edited frame.
+/// It is a pointer to consecutive CUBE_FRAME_SIZE bytes.
 uint8_t* cube_get_frame(void);
 
-/**
- * Turns off the cube and related interrupts.
- */
+/// Turns off the cube and related interrupts.
 void cube_disable(void);
 
-/**
- * Turns on the cube and related interrupts and start processing the frame buffer.
- */
+/// Turns on the cube and related interrupts and start processing the frame buffer.
 void cube_enable(void);
 
-#endif /* CUBE_H */
+#endif // CUBE_H
+
