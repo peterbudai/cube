9aa8c0d8fccd2208eb6905b53bd917db60ac6cee
Péter Budai <buc@peterbudai.eu>
2017-04-05 22:34:46 +0200

    Making UI code a bit prettier

--- test/src/ui.c
+++ test/src/ui.c
@@ -21,8 +21,13 @@
 float LED_DIM[DIM_MAX] = {0.8, 1, 0.8};
 float LED_OFF[DIM_MAX] = {0.6, 1, 0.6};
 
+uint64_t last_time = 0;
+uint64_t last_ticks = 0;
+uint64_t last_draw = 0;
+uint64_t last_uart[2] = {0, 0};
+
 uint64_t draw_count = 0;
-char status[512] = {'\0'};
+char status_lines[3][512] = {"", "", ""};
 
 int rotate_x = 11, rotate_y = -23;
 int drag_x = 0, drag_y = 0;
@@ -114,14 +119,16 @@ static void draw_ligths() {
 }
 
 static void draw_info() {
-	int len = strlen(status);
-	glWindowPos2i(1000 - len * 9, 1000 - 15);
-	for(int i = 0; i < len; ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, status[i]);
+	for(int i = 0; i < 3; ++i) {
+		int len = strlen(status_lines[i]);
+		glWindowPos2i(1000 - len * 9, 1000 - 15 * (i + 1));
+		for(int j = 0; j < len; ++j) {
+			glutBitmapCharacter(GLUT_BITMAP_9_BY_15, status_lines[i][j]);
+		}
 	}
 }
 
-void onDisplay() {
+static void draw_callback() {
 	glClearColor(31/255.0,79/255.0,111/255.0,1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
@@ -137,7 +144,7 @@ void onDisplay() {
 	draw_count++;
 }
 
-void onMouse(int button, int state, int x, int y) {
+static void mouse_click_callback(int button, int state, int x, int y) {
 	if(button == GLUT_LEFT_BUTTON) {
 		if(state == GLUT_DOWN) {
 			drag_on = true;
@@ -149,7 +156,7 @@ void onMouse(int button, int state, int x, int y) {
 	}
 }
 
-void onMotion(int x, int y) {
+static void mouse_move_callback(int x, int y) {
 	if(drag_on) {
 		int diffX = x - drag_x;
 		if(abs(diffX) > 10) {
@@ -173,14 +180,9 @@ void onMotion(int x, int y) {
 	}
 }
 
-void onTimer(int value __attribute__((unused))) {
-	static unsigned long last_time = 0;
-	static uint64_t last_ticks = 0;
-	static uint64_t last_draw = 0;
-	static uint64_t last_uart[2] = {0, 0};
-
-	unsigned long cur_time = glutGet(GLUT_ELAPSED_TIME);
-	unsigned long elapsed_time = cur_time - last_time;
+static void timer_callback(int value __attribute__((unused))) {
+	uint64_t current_time = glutGet(GLUT_ELAPSED_TIME);
+	uint64_t elapsed_time = current_time - last_time;
 
 	leds_dim_down();
 
@@ -191,7 +193,7 @@ void onTimer(int value __attribute__((unused))) {
 		uint64_t current_uart[2];
 		uart_get_counts(current_uart);
 
-		float real = (float)cur_time / 1000;
+		float real = (float)current_time / 1000;
 		float virt = (float)mcu_ticks / MCU_FREQ;
 		float cps = (float)elapsed_ticks / elapsed_time / 1000;
 		float ratio = (float)elapsed_ticks / MCU_FREQ * 100;
@@ -203,15 +205,17 @@ void onTimer(int value __attribute__((unused))) {
 			bps[i] = (float)elapsed_uart / elapsed_time * 1000;
 			last_uart[i] = current_uart[i];
 		}
-		snprintf(status, sizeof(status), "Real: %.1f s, Virt: %.1f s, CPU: %.2f MHz (%.1f %%), In: %lu B (%.1f Bps), Out: %lu B (%.1f Bps), Sim: %.2f FPS", real, virt, cps, ratio, current_uart[UART_INPUT], bps[UART_INPUT], current_uart[UART_OUTPUT], bps[UART_OUTPUT], fps);
-
-		last_time = cur_time;
+		last_time = current_time;
 		last_ticks = mcu_ticks;
 		last_draw = draw_count;
+
+		snprintf(status_lines[0], 512, "Real time: %.1f s, Virtual time: %.1f s, CPU speed: %.2f MHz (%.1f %%)", real, virt, cps, ratio);
+		snprintf(status_lines[1], 512, "UART in: %lu B (%.1f Bps), UART out: %lu B (%.1f Bps)", current_uart[UART_INPUT], bps[UART_INPUT], current_uart[UART_OUTPUT], bps[UART_OUTPUT]);
+		snprintf(status_lines[2], 512, "Display: %.2f FPS", fps);
 	}
 
 	glutPostRedisplay();
-	glutTimerFunc(20, onTimer, 0);
+	glutTimerFunc(1000 / 50, timer_callback, 0);
 }
 
 void ui_init(int* argc, char** argv) {
@@ -219,10 +223,10 @@ void ui_init(int* argc, char** argv) {
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
 	glutInitWindowSize(1000, 1000);
 	glutCreateWindow("LED Cube Simulator");
-	glutDisplayFunc(onDisplay);
-	glutMouseFunc(onMouse);
-	glutMotionFunc(onMotion);
-	glutTimerFunc(1, onTimer, 0);
+	glutDisplayFunc(draw_callback);
+	glutMouseFunc(mouse_click_callback);
+	glutMotionFunc(mouse_move_callback);
+	glutTimerFunc(1, timer_callback, 0);
 
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
